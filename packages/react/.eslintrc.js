1a298b8cceac404e3d5faed576d54db187b89b13
Karibash <y.karibayashi@gmail.com>
2023-10-04 16:44:13 +0900

    [feature] Add a react config package

---
+++ packages/react/.eslintrc.js
@@ -0,0 +1,7 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  'extends': './index.js',
+  'env': {
+    node: true,
+  },
+};
---
+++ packages/react/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @agaroot/eslint-config-react
+
+## 1.0.0
+
+### Major Changes
+
+- First release.
