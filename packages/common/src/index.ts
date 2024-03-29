f9c4ae816b80f56e6e2d12aee5cd4ce56f26ab19
Karibash <y.karibayashi@gmail.com>
2024-03-29 23:45:24 +0900

    feature: Add linter options

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,9 +1,11 @@
 import { ignoreFactory } from './factories/ignore-factory';
+import { linterFactory } from './factories/linter-factory';
 
 import type { Linter } from 'eslint';
 
 export const common = (): Linter.FlatConfig[] => {
   return [
     ...ignoreFactory(),
+    ...linterFactory(),
   ];
 };
