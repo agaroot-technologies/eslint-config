b09e3d4f94fd732e3080e762a4aea38d8a0237f8
Karibash <y.karibayashi@gmail.com>
2024-04-19 20:17:06 +0900

    feature: Add a import plugin

--- packages/javascript/src/index.ts
+++ packages/javascript/src/index.ts
@@ -1,4 +1,5 @@
 import { globalsFactory } from './factories/globals-factory';
+import { importFactory } from './factories/import-factory';
 import { javascriptFactory } from './factories/javascript-factory';
 
 import type { Linter } from 'eslint';
@@ -6,6 +7,7 @@ import type { Linter } from 'eslint';
 export const javascript = (): Linter.FlatConfig[] => {
   return [
     ...globalsFactory(),
+    ...importFactory(),
     ...javascriptFactory(),
   ];
 };
