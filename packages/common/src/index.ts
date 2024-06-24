07375f501ab8e7a4df3dc62d5aecd7a9e00d8b20
Karibash <y.karibayashi@gmail.com>
2024-06-25 01:19:48 +0900

    feature: Add a eslint-plugin-jsonc

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,9 +1,11 @@
 import { define } from '@agaroot/eslint-config-definer';
 
 import { ignoreConfigurator } from './configurators/ignore-configurator';
+import { jsonConfigurator } from './configurators/json-configurator';
 import { linterConfigurator } from './configurators/linter-configurator';
 
 export const common = define([
   ignoreConfigurator,
+  jsonConfigurator,
   linterConfigurator,
 ]);
