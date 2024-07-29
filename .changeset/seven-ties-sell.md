72214eeb41a7c0c8a1c6448c9f5467af6a6d0aab
Karibash <y.karibayashi@gmail.com>
2024-07-29 21:15:45 +0900

    feature: Make it possible to pass a config instead of a function

---
+++ .changeset/seven-ties-sell.md
@@ -0,0 +1,5 @@
+---
+"@agaroot/eslint-config-definer": patch
+---
+
+Make it possible to pass a config instead of a function
--- packages/definer/src/index.ts
+++ packages/definer/src/index.ts
@@ -2,11 +2,12 @@ import type { Linter } from 'eslint';
 
 export type Configurator<T = void> =
   T extends void
-    ? () => Linter.FlatConfig | Linter.FlatConfig[]
-    : (options: T) => Linter.FlatConfig | Linter.FlatConfig[];
+    ? (() => Linter.FlatConfig | Linter.FlatConfig[]) | Linter.FlatConfig | Linter.FlatConfig[]
+    : ((options: T) => Linter.FlatConfig | Linter.FlatConfig[]) | Linter.FlatConfig | Linter.FlatConfig[];
 
 export const define = <T>(configurators: Configurator<T>[]): (options: T) => Linter.FlatConfig[] => {
-  return (options) => configurators.flatMap((configurator) => {
+  return (options) => configurators.flatMap<Linter.FlatConfig>((configurator) => {
+    if (typeof configurator === 'object') return configurator;
     return configurator(options);
   });
 };
