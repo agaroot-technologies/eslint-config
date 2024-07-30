d00880c8d31cadeb0f0cb226d3b51c7cae6ceabe
Karibash <y.karibayashi@gmail.com>
2024-07-30 12:28:23 +0900

    feature: Make the options be merged when options of different types are passed

---
+++ .changeset/quick-mirrors-shop.md
@@ -0,0 +1,5 @@
+---
+"@agaroot/eslint-config-definer": patch
+---
+
+Make the options be merged when options of different types are passed
--- packages/definer/src/index.ts
+++ packages/definer/src/index.ts
@@ -1,12 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Linter } from 'eslint';
+
+type ExtractOptionType<T> = T extends Configurator<infer U> ? U : never;
+
+type UnionToIntersection<U> =
+  (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
+
+export type MergeOptionTypes<T extends any[]> = UnionToIntersection<ExtractOptionType<T[number]>>;
 
 export type Configurator<T = void> =
   T extends void
-    ? (() => Linter.FlatConfig | Linter.FlatConfig[]) | Linter.FlatConfig | Linter.FlatConfig[]
-    : ((options: T) => Linter.FlatConfig | Linter.FlatConfig[]) | Linter.FlatConfig | Linter.FlatConfig[];
+    ? (() => Linter.FlatConfig | Linter.FlatConfig[])
+    : ((options: T) => Linter.FlatConfig | Linter.FlatConfig[]);
 
-export const define = <T>(configurators: Configurator<T>[]): (options: T) => Linter.FlatConfig[] => {
-  return (options) => configurators.flatMap<Linter.FlatConfig>((configurator) => {
+export const define = <T extends Configurator<any>[]>(
+  configurators: T,
+): (options: MergeOptionTypes<T>) => Linter.FlatConfig[] => {
+  return (options) => configurators.flatMap((configurator) => {
     if (typeof configurator === 'object') return configurator;
     return configurator(options);
   });
